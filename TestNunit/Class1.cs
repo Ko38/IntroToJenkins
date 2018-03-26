44d7f97d7699abe426d19c056c9dd62ea0fb18aa
Ko38 <phillipko38@outlook.com>
2018-03-26 18:01:40 +0800

    Pass

---
+++ TestNunit/Class1.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNunit
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.That(1, Is.EqualTo(1));
+        }
+    }
+}
