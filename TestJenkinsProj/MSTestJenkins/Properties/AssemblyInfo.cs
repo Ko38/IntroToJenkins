2040717b5764c95b32a4e6b55388d0dfb58e161a
ko38 <phillipko38@outlook.com>
2018-03-26 09:55:03 +0800

    Proj

---
+++ TestJenkinsProj/MSTestJenkins/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MSTestJenkins")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MSTestJenkins")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("04bed0cd-5622-439a-a2cd-4559a09c4551")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestJenkinsProj/MSTestJenkins/Test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MSTestJenkins
+{
+    [TestClass]
+    public class Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(Math.Add(1, 2), 3);
+        }
+    }
+
+    public class Math
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
